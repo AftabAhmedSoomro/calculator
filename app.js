a43c668dbf5c3323f7cecc5d61c966ac9064b80c
AftabAhmedSoomro <154562779+AftabAhmedSoomro@users.noreply.github.com>
2024-03-07 02:42:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let number1 = +prompt("Enter 1st number");
+let number2 = +prompt("Enter 2nd number");
+let sum = number1 + number2;
+document.write("The sum of two numbers is: " , sum , "<br/>");
+
+let sub = number1 - number2;
+document.write("The subtraction of two numbers is: " , sub , "<br/>");
+
+let mult = number1 * number2;
+document.write("The multiplication between two numbers is: " , mult , "<br/>");
+
+let div = number1 / number2;
+document.write("The division between two numbers is: " , div , "<br/>");
+
+let rem = number1 % number2;
+document.write("The remainder of two numbers is: " , rem , "<br/>");
